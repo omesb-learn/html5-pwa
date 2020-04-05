1078e44d5d1a6145d627265925234732bfb01c9f
omesb <36600384+omesb@users.noreply.github.com>
2020-04-05 15:01:25 +0200

    fix sw

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // register the service-worker.js
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js', {scope: '/'});
+    navigator.serviceWorker.register('/service-worker.js');
 }
